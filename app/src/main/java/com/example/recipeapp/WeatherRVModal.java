27dbd5b853316596d475e2d2e37578cad44ec04b
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-09-15 10:02:15 +0530

    Create WeatherRVModal.java

---
+++ app/src/main/java/com/example/recipeapp/WeatherRVModal.java
@@ -0,0 +1,50 @@
+package com.example.recipeapp;
+
+public class WeatherRVModal {
+
+    public String time;
+    public String temperature;
+    public String icon;
+    public String windSpeed;
+
+    public WeatherRVModal(String time, String temperature, String icon, String windSpeed) {
+        this.time = time;
+        this.temperature = temperature;
+        this.icon = icon;
+        this.windSpeed = windSpeed;
+    }
+
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+}
