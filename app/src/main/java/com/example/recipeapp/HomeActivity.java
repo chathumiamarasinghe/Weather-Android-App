53891d1eb350fabbef036fe888565bdcb7b96b50
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-09-17 13:04:58 +0530

    Update HomeActivity.java

--- app/src/main/java/com/example/recipeapp/HomeActivity.java
+++ app/src/main/java/com/example/recipeapp/HomeActivity.java
@@ -1,6 +1,14 @@
 package com.example.recipeapp;
+import android.Manifest;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -9,6 +17,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,6 +26,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.Manifest;
+import android.widget.Toast;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -28,6 +43,8 @@ public class HomeActivity extends AppCompatActivity {
     private ImageView blackShade,SearchIcon,WeatherIcon;
     private ArrayList WeatherRVModalArrayList;
     private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE =1;
 
 
     @Override
@@ -50,12 +67,44 @@ public class HomeActivity extends AppCompatActivity {
         weatherRVAdapter = new WeatherRVAdapter(this, WeatherRVModalArrayList);
         recyclerView.setAdapter(weatherRVAdapter);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(HomeActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private String getCityName(double longitude,double latitude){
+        String cityName = "Not Found";
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try{
+            List<Address> addresses = gcd.getFromLocation(latitude,longitude,10);
+            for (Address adr : addresses) {
+                if (adr != null) {
+                    String city = adr.getLocality();
+                    if (city != null && !city.equals("")) {
+                        cityName = city;
+                    }else{
+                        Log.d("TAG", "CITY NOT FOUND");
+                        Toast.makeText(this, "CITY NOT FOUND", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return cityName;
     }
 
     private void getWeatherData(String cityName) {
